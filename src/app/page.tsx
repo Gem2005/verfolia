11a82e4ddd0b0399fcf98451ee3eaf6b87ee062c
Melroyjoanes <melroy@verfolia.com>
2025-09-03 18:53:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,14 +71,14 @@ const HomePage = () => {
                 >
                   <Rocket className="w-5 h-5 mr-2" />
                   <Link href="/create-resume">Get Started Free</Link>
-                </Button>
-                <Button
-                  size="lg"
-                  className="text-lg px-8 py-4 h-auto bg-primary text-primary-foreground hover:opacity-90 transition-all duration-200"
-                >
-                  <Upload className="w-5 h-5 mr-2" />
-                  <Link href="/upload-resume">Upload PDF</Link>
-                </Button>
+               <Button
+  size="lg"
+  className="text-lg px-8 py-4 h-auto bg-primary text-primary-foreground hover:opacity-90 transition-all duration-200"
+>
+  <Upload className="w-5 h-5 mr-2" />
+  {/* CHANGE THIS LINE */}
+  <Link href="/create-resume">Upload PDF</Link>
+</Button>
               </div>
 
               {/* Trust Indicators */}
