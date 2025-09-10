321224a5e3f3dac3a27994c8ca2435766f645dda
Melroyjoanes <melroy@verfolia.com>
2025-09-10 15:24:37 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ const HomePage = () => {
 >
   <Link href="/upload-resume">  {/* ✅ FIXED */}
     <Upload className="w-5 h-5 mr-2" />
-    Upload PDF
+    Upload Resume
   </Link>
 </Button>
               </div>
